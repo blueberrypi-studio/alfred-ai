1deda6f8031c10dc8f0094408a9bdd8728e6a89d
RainbowExpert41 <handyhaydn22@gmail.com>
2022-12-28 22:07:28 +1300

    Created the basic GUI, zero implementation to bot

--- v2/gui.py
+++ v2/gui.py
@@ -1,11 +1,3 @@
-"""
-Alfred AI version 2
-same as version 1, built into a gui
-@author: Haydn Boul
-@date: 27-12-22
-"""
-
-import threading
 import tkinter as tk
 
 # TODO: Remove this to a .conf or similar file
@@ -18,37 +10,49 @@ BOT_NAME = "Alfred"
 BACKGROUND_COLOUR = "black"
 FOREGROUND_COLOUR = "white"
 
+class Application(tk.Frame):
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title(WINDOW_TITLE)
+        self.root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+        # keep on top of other windows, regardless of focus  
+        if HOLD_TOP_LAYER: self.root.attributes("-topmost", 1)
 
-def setup():
-    """Setups the window"""
-    alfred = tk.Tk()
-    alfred.title(WINDOW_TITLE)
-    alfred.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
-    # keep on top of other windows, regardless of focus  
-    if HOLD_TOP_LAYER: alfred.attributes("-topmost", 1) 
+        tk.Frame.__init__(self, self.root)
+        self.create_widgets()
 
-    return alfred
+    def create_widgets(self):
+        
+        self.main_container = tk.Frame(bg="purple", padx=5, pady=5)
+        self.heading_container = tk.Frame(master=self.main_container, bg="blue", padx=5, pady=5)
+        self.response_container = tk.Frame(master=self.main_container, bg="green", padx=5, pady=5)
+        self.entry_container = tk.Frame(master=self.main_container, bg="yellow")
+
+        self.name_tag = tk.Label(master=self.heading_container, text=BOT_NAME, bg=BACKGROUND_COLOUR, fg=FOREGROUND_COLOUR, font=("Arial", 25))
+        self.name_tag.pack()
+
+        self.response_field = tk.Label(master=self.response_container, text="Answer goes here")
+        self.response_field.pack()
+        
+        self.entry_field = tk.Entry(master=self.entry_container, width=(WINDOW_WIDTH))
+        self.entry_field.pack()
+        self.root.bind('<Return>', self.parse)
+        
+
+        self.main_container.pack(fill="both", expand=True)
+        self.heading_container.pack(fill="both")
+        self.response_container.pack(fill="both", expand=True)
+        self.entry_container.pack(fill="both", expand=False)
+
+        # change window icon
+        p1 = tk.PhotoImage(file = 'v2/images/icon.png')
+        self.root.iconphoto(False, p1)
+
+    def parse(self, event):
+        self.response_field.config(text=self.entry_field.get())
+
+    def start(self):
+        self.root.mainloop()
 
 
-def draw_gui(alfred):
-    main_container = tk.Frame(alfred, bg=BACKGROUND_COLOUR).pack(fill="both", expand=True)
-
-    name_tag = tk.Label(main_container, text=BOT_NAME, bg=BACKGROUND_COLOUR, fg=FOREGROUND_COLOUR, font=("Arial", 25))
-    name_tag.pack()
-
-
-
-
-
-
-def main():
-    alfred = setup()
-    
-    draw_gui(alfred)
-    alfred.mainloop()
-
-
-if __name__ == "__main__":
-    main()
-
-
+Application().start()
