87f7cbb5a45c3814eb7e5184c6fe027b9da3e13c
Haydn Boul <--global>
2023-01-01 14:43:44 +1300

    added nhl draw widget method

--- v2/skills/nhl.py
+++ v2/skills/nhl.py
@@ -16,13 +16,34 @@ class Todays_Schedule(A_Skill):
         self.widget_frame = tk.Frame(self.gui.main_container, bg="white", padx=5, pady=5)
         self.content_frame = tk.Frame(self.widget_frame, bg="black")
         
-        self.widget_title = tk.Label(self.content_frame, text=self.skill_name).pack()
-        for game in game_list:
-            widget_label = tk.Label(self.content_frame, text=game, bg="black", fg="white")
-            widget_label.pack()
+        self.widget_title = tk.Label(self.content_frame, text=self.skill_name).grid(row=0, column=0, columnspan=2)
+        
+        row = 1
+        column = 0
 
-        self.widget_frame.place(x=100, y=100)
+        for game in game_list:
+            game_box = tk.Frame(self.content_frame, padx=5, pady=5)
+            time_label = tk.Label(game_box, text=f"{game[0]}: {game[1]}")
+            time_label.pack()
+            home_team_label = tk.Label(game_box, text=game[2], bg="black", fg="white", padx=5, pady=5)
+            home_team_label.pack()
+
+            away_team_label = tk.Label(game_box, text=game[3], bg="black", fg="white", padx=5, pady=5)
+            away_team_label.pack()
+
+            game_box.grid(row=row, column=column)
+
+            column += 1
+            if column >= 2:
+                column = 0
+                row += 1
+
         self.content_frame.pack()
+        self.widget_frame.place(x=100, y=100)
+        
+            
+
+        
         
         
     def todays_schedule(self):
@@ -33,16 +54,24 @@ class Todays_Schedule(A_Skill):
         games_list = sub_dict['games']
 
         for game in games_list:
-            game = game["teams"]
-            home_team = game['home']['team']['name']
-            away_team = game['away']['team']['name']
+            data = game["linescore"]
 
-            home_score = game['home']['score']
-            away_score = game['away']['score']
+            home_team = data['teams']['home']['team']['name']
+            away_team = data['teams']['away']['team']['name']
 
-            game_score = (f"{home_team}: {home_score}", f"{away_team}: {away_score}")
+            home_score = data['teams']['home']['goals']
+            away_score = data['teams']['away']['goals']
+
+            period = data['currentPeriod']
+            if period == 0:
+                time="not yet started"
+            else:
+                time = data['currentPeriodTimeRemaining']
+
+
+            game_score = (f"{period}",f"{time}", f"{home_team}: {home_score}", f"{away_team}: {away_score}")
             todays_games.append(game_score)
-        
+
         if self.DEBUG == True:
             for game in todays_games:
                 print(game)
