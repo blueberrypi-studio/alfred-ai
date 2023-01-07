17839dabb4f753e80192cabf75d8a457d0714c11
Haydn Boul <handyhaydn22@gmail.com>
2023-01-07 22:07:48 +1300

    added date widget

--- v2/skills/date_time.py
+++ v2/skills/date_time.py
@@ -36,6 +36,38 @@ class Time(A_Skill): # note capitalisation
 
         self.widget_frame.place(x=1000, y=100)
         self.content_frame.pack()
+
+
+class Date(A_Skill):
+    __custom__ = True
+
+    def date(self): 
+        self.set_name("Date Module")
+        self.draw_widget()
+        string = strftime('%A\n%-d %B %Y')
+        self.date_text.config(text=string)
+        self.date_text.after(1000, self.update_date)
+        
+        if self.DEBUG == True:
+            print(string)
+
+    def update_date(self):
+        """updates the time label"""
+        string = strftime('%A\n%-d %B %Y')
+        self.date_text.config(text=string)
+        self.date_text.after(1000, self.update_date)
+
+
+    def draw_widget(self):
+        """draw a widget, can exclude this if needed (in parent class also)"""
+        self.widget_frame = tk.Frame(self.gui.main_container, bg=self.widget_colour, padx=5, pady=5)
+        self.content_frame = tk.Frame(self.widget_frame, bg=self.background_colour)
+        
+        self.date_text = tk.Label(self.content_frame, text="", font=("Arial", 25), fg=self.foreground_colour, bg=self.background_colour, padx=5, pady=5)
+        self.date_text.pack()
+
+        self.widget_frame.place(x=1000, y=350)
+        self.content_frame.pack()
         
 
 if __name__ == '__main__':
